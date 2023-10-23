9a5dfe126406310dc561bb0134bccc027ec1d35b
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-10-23 22:59:32 +0530

    Update register.php

--- register.php
+++ register.php
@@ -30,7 +30,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>RailMumbai Registration</title>
+	<title>RailIndia Registration</title>
 	<link rel="shortcut icon" href="assets/favicon.png">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
