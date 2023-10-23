592e2b15e4b6ddccf8bd67a56ec3f1a87c77adc2
Sanket Sarjerao More-Patil <sp3130016@gmail.com>
2023-10-23 23:00:55 +0530

    Update transactions.php

--- transactions.php
+++ transactions.php
@@ -19,7 +19,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>RailMumbai User Transaction</title>
+	<title>RailIndia User Transaction</title>
 	<link rel="shortcut icon" href="assets/favicon.png">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
